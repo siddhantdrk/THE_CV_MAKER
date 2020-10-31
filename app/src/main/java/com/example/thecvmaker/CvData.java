d3ce3be09edd89ae951211b57e58f75cd47864c5
Rahul Dev <iit2019053@iiita.ac.in>
2020-10-31 17:40:46 +0530

    added database

---
+++ app/src/main/java/com/example/thecvmaker/CvData.java
@@ -0,0 +1,396 @@
+package com.example.thecvmaker;
+
+public class CvData {
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+    private String dob;
+    private String nationality;
+    private String gender;
+    private String language;
+    private String schoolName;
+    private String collegeName;
+    private String percentage;
+    private String passingYear;
+    private String designation;
+    private String organisationName;
+    private String workingPeriod;
+    private String interestedField;
+    private String skills;
+    private String hobby;
+    private String strength;
+    private String achievement;
+    private String carrierObjective;
+    private String title;
+    private String period;
+    private String role;
+    private String description;
+    private String teamSize;
+    public CvData(int id, String name, String address, String phoneNumber, String emailAddress,
+                   String dob,String nationality,String gender,String language,String schoolName,
+                   String collegeName,String percentage,String passingYear,String designation,
+                   String organisationName,String workingPeriod,String interestedField,String skills,
+                   String hobby,String strength,String achievement,String carrierObjective,String title,
+                   String period,String role,String description,String teamSize){
+        this.id=id;
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.emailAddress=emailAddress;
+        this. dob=dob;
+        this.nationality=nationality;
+        this.gender=gender;
+        this.language=language;
+        this.schoolName=schoolName;
+        this.collegeName=collegeName;
+        this.percentage=percentage;
+        this.passingYear=passingYear;
+        this.designation=designation;
+        this.organisationName=organisationName;
+        this.workingPeriod=workingPeriod;
+        this.interestedField=interestedField;
+        this.skills=skills;
+        this.hobby=hobby;
+        this.strength=strength;
+        this.achievement=achievement;
+        this.carrierObjective=carrierObjective;
+        this.title=title;
+        this.period=period;
+        this.role=role;
+        this.description=description;
+        this.teamSize=teamSize;
+
+    }
+    public CvData(String name, String address, String phoneNumber, String emailAddress,
+                   String dob,String nationality,String gender,String language,String schoolName,
+                   String collegeName,String percentage,String passingYear,String designation,
+                   String organisationName,String workingPeriod,String interestedField,String skills,
+                   String hobby,String strength,String achievement,String carrierObjective,String title,
+                   String period,String role,String description,String teamSize){
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.emailAddress=emailAddress;
+        this. dob=dob;
+        this.nationality=nationality;
+        this.gender=gender;
+        this.language=language;
+        this.schoolName=schoolName;
+        this.collegeName=collegeName;
+        this.percentage=percentage;
+        this.passingYear=passingYear;
+        this.designation=designation;
+        this.organisationName=organisationName;
+        this.workingPeriod=workingPeriod;
+        this.interestedField=interestedField;
+        this.skills=skills;
+        this.hobby=hobby;
+        this.strength=strength;
+        this.achievement=achievement;
+        this.carrierObjective=carrierObjective;
+        this.title=title;
+        this.period=period;
+        this.role=role;
+        this.description=description;
+        this.teamSize=teamSize;
+    }
+    public CvData(){
+
+    }
+
+    public CvData(String name, String address, String phoneNumber, String emailAddress,
+                  String dob,String nationality,String gender,String language){
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.emailAddress=emailAddress;
+        this. dob=dob;
+        this.nationality=nationality;
+        this.gender=gender;
+        this.language=language;
+    }
+    public CvData(int id,String name, String address, String phoneNumber, String emailAddress,
+                  String dob,String nationality,String gender,String language){
+        this.name=name;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+        this.emailAddress=emailAddress;
+        this. dob=dob;
+        this.nationality=nationality;
+        this.gender=gender;
+        this.language=language;
+    }
+    public CvData(String schoolName,String collegeName,String percentage,String passingYear){
+        this.schoolName=schoolName;
+        this.collegeName=collegeName;
+        this.percentage=percentage;
+        this.passingYear=passingYear;
+    }
+    public CvData(int id,String schoolName,String collegeName,String percentage,String passingYear){
+        this.schoolName=schoolName;
+        this.collegeName=collegeName;
+        this.percentage=percentage;
+        this.passingYear=passingYear;
+    }
+    public CvData(String designation,String organisationName,String workingPeriod){
+        this.designation=designation;
+        this.organisationName=organisationName;
+        this.workingPeriod=workingPeriod;
+    }
+    public CvData(int id,String designation, String organisationName,String workingPeriod){
+        this.designation=designation;
+        this.organisationName=organisationName;
+        this.workingPeriod=workingPeriod;
+    }
+    public CvData(String interestedField,String skills,
+                  String hobby,String strength,String achievement,String carrierObjective){
+        this.interestedField=interestedField;
+        this.skills=skills;
+        this.hobby=hobby;
+        this.strength=strength;
+        this.achievement=achievement;
+        this.carrierObjective=carrierObjective;
+    }
+    public CvData(int id,String interestedField,String skills,
+                  String hobby,String strength,String achievement,String carrierObjective){
+        this.interestedField=interestedField;
+        this.skills=skills;
+        this.hobby=hobby;
+        this.strength=strength;
+        this.achievement=achievement;
+        this.carrierObjective=carrierObjective;
+    }
+    public CvData(String title,String period,String role,String description,String teamSize){
+        this.title=title;
+        this.period=period;
+        this.role=role;
+        this.description=description;
+        this.teamSize=teamSize;
+    }
+    public CvData(int id,String title,String period,String role,String description,String teamSize){
+        this.title=title;
+        this.period=period;
+        this.role=role;
+        this.description=description;
+        this.teamSize=teamSize;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(String percentage) {
+        this.percentage = percentage;
+    }
+
+    public String getPassingYear() {
+        return passingYear;
+    }
+
+    public void setPassingYear(String passingYear) {
+        this.passingYear = passingYear;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getOrganisationName() {
+        return organisationName;
+    }
+
+    public void setOrganisationName(String organisationName) {
+        this.organisationName = organisationName;
+    }
+
+    public String getWorkingPeriod() {
+        return workingPeriod;
+    }
+
+    public void setWorkingPeriod(String workingPeriod) {
+        this.workingPeriod = workingPeriod;
+    }
+
+    public String getInterestedField() {
+        return interestedField;
+    }
+
+    public void setInterestedField(String interestedField) {
+        this.interestedField = interestedField;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public void setStrength(String strength) {
+        this.strength = strength;
+    }
+
+    public String getAchievement() {
+        return achievement;
+    }
+
+    public void setAchievement(String achievement) {
+        this.achievement = achievement;
+    }
+
+    public String getCarrierObjective() {
+        return carrierObjective;
+    }
+
+    public void setCarrierObjective(String carrierObjective) {
+        this.carrierObjective = carrierObjective;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTeamSize() {
+        return teamSize;
+    }
+
+    public void setTeamSize(String teamSize) {
+        this.teamSize = teamSize;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+}
---
+++ app/src/main/java/com/example/thecvmaker/MyDbHelper.java
@@ -0,0 +1,167 @@
+package com.example.thecvmaker;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+
+    public MyDbHelper(Context context) {
+        super(context, Params.KEY_DATABASE, null, Params.KEY_DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + Params.TABLE_NAME1 + "("
+                + Params.KEY_ID + " INTEGER PRIMARY KEY," + Params.KEY_NAME + " TEXT,"
+                + Params.KEY_EMAIL + " TEXT," + Params.KEY_PHONE_NUMBER + " TEXT," + Params.KEY_DOB + " TEXT,"
+                + Params.KEY_GENDER + " TEXT," + Params.KEY_NATIONALITY + " TEXT," + Params.KEY_ADDRESS + " TEXT,"
+                + Params.KEY_LANGUAGE + " TEXT," + Params.KEY_SCHOOL_NAME + " TEXT," + Params.KEY_COLLEGE_NAME + " TEXT,"
+                + Params.KEY_SCORE_PERCENTAGE + " TEXT," + Params.KEY_YEAR_OF_PASSING + " TEXT," + Params.KEY_DESIGNATION + " TEXT,"
+                + Params.KEY_ORGANISATION_NAME + " TEXT," + Params.KEY_WORKING_PERIOD + " TEXT," + Params.KEY_FIELD_OF_INTEREST + " TEXT,"
+                + Params.KEY_SKILLS + " TEXT," + Params.KEY_HOBBY + " TEXT," + Params.KEY_STRENGTH + " TEXT,"
+                + Params.KEY_ACHIEVEMENT + " TEXT," + Params.KEY_CARRIER_OBJECTIVE + " TEXT," + Params.KEY_TITLE + " TEXT,"
+                + Params.KEY_PERIOD + " TEXT," +Params.KEY_ROLE + " TEXT," + Params.KEY_DESCRIPTION + " TEXT," + Params.KEY_TEAM_SIZE +")";
+
+        Log.d("dbharry", "Query being run is : "+ create);
+        db.execSQL(create);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addContact(CvData cvData){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Params.KEY_NAME, cvData.getName());
+        values.put(Params.KEY_EMAIL, cvData.getEmailAddress());
+        values.put(Params.KEY_PHONE_NUMBER, cvData.getPhoneNumber());
+        values.put(Params.KEY_DOB, cvData.getDob());
+        values.put(Params.KEY_GENDER, cvData.getGender());
+        values.put(Params.KEY_NATIONALITY, cvData.getNationality());
+        values.put(Params.KEY_ADDRESS, cvData.getAddress());
+        values.put(Params.KEY_LANGUAGE, cvData.getLanguage());
+        values.put(Params.KEY_SCHOOL_NAME, cvData.getSchoolName());
+        values.put(Params.KEY_COLLEGE_NAME, cvData.getCollegeName());
+        values.put(Params.KEY_SCORE_PERCENTAGE, cvData.getPercentage());
+        values.put(Params.KEY_YEAR_OF_PASSING, cvData.getPassingYear());
+        values.put(Params.KEY_DESIGNATION, cvData.getDesignation());
+        values.put(Params.KEY_ORGANISATION_NAME, cvData.getOrganisationName());
+        values.put(Params.KEY_WORKING_PERIOD, cvData.getWorkingPeriod());
+        values.put(Params.KEY_FIELD_OF_INTEREST, cvData.getInterestedField());
+        values.put(Params.KEY_SKILLS, cvData.getSkills());
+        values.put(Params.KEY_HOBBY, cvData.getHobby());
+        values.put(Params.KEY_STRENGTH, cvData.getStrength());
+        values.put(Params.KEY_ACHIEVEMENT, cvData.getAchievement());
+        values.put(Params.KEY_CARRIER_OBJECTIVE, cvData.getCarrierObjective());
+        values.put(Params.KEY_TITLE, cvData.getTitle());
+        values.put(Params.KEY_PERIOD,cvData.getPeriod());
+        values.put(Params.KEY_ROLE, cvData.getRole());
+        values.put(Params.KEY_DESCRIPTION, cvData.getDescription());
+        values.put(Params.KEY_TEAM_SIZE, cvData.getTeamSize());
+
+        db.insert(Params.TABLE_NAME1, null, values);
+        Log.d("dev", "Successfully inserted");
+        db.close();
+
+
+    }
+
+    public List<CvData> getAllContacts(){
+        List<CvData> contactList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Generate the query to read from the database
+        String select = "SELECT * FROM " + Params.TABLE_NAME1;
+        Cursor cursor = db.rawQuery(select, null);
+        //Loop through now
+        if(cursor.moveToFirst()){
+            do{
+                CvData contact = new CvData();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setEmailAddress(cursor.getString(2));
+                contact.setPhoneNumber(cursor.getString(2));
+                contact.setDob(cursor.getString(2));
+                contact.setGender(cursor.getString(2));
+                contact.setNationality(cursor.getString(2));
+                contact.setAddress(cursor.getString(2));
+                contact.setLanguage(cursor.getString(2));
+                contact.setSchoolName(cursor.getString(2));
+                contact.setCollegeName(cursor.getString(2));
+                contact.setPercentage(cursor.getString(2));
+                contact.setPassingYear(cursor.getString(2));
+                contact.setDesignation(cursor.getString(2));
+                contact.setOrganisationName(cursor.getString(2));
+                contact.setWorkingPeriod(cursor.getString(2));
+                contact.setInterestedField(cursor.getString(2));
+                contact.setSkills(cursor.getString(2));
+                contact.setHobby(cursor.getString(2));
+                contact.setStrength(cursor.getString(2));
+                contact.setAchievement(cursor.getString(2));
+                contact.setCarrierObjective(cursor.getString(2));
+                contact.setTitle(cursor.getString(2));
+                contact.setPeriod(cursor.getString(2));
+                contact.setRole(cursor.getString(2));
+                contact.setDescription(cursor.getString(2));
+                contact.setTeamSize(cursor.getString(2));
+                contactList.add(contact);
+            }while(cursor.moveToNext());
+        }
+        return contactList;
+    }
+
+    public int updateContact(CvData cvData){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Params.KEY_NAME, cvData.getName());
+        values.put(Params.KEY_EMAIL, cvData.getEmailAddress());
+        values.put(Params.KEY_PHONE_NUMBER, cvData.getPhoneNumber());
+        values.put(Params.KEY_DOB, cvData.getDob());
+        values.put(Params.KEY_GENDER, cvData.getGender());
+        values.put(Params.KEY_NATIONALITY, cvData.getNationality());
+        values.put(Params.KEY_ADDRESS, cvData.getAddress());
+        values.put(Params.KEY_LANGUAGE, cvData.getLanguage());
+        values.put(Params.KEY_SCHOOL_NAME, cvData.getSchoolName());
+        values.put(Params.KEY_COLLEGE_NAME, cvData.getCollegeName());
+        values.put(Params.KEY_SCORE_PERCENTAGE, cvData.getPercentage());
+        values.put(Params.KEY_YEAR_OF_PASSING, cvData.getPassingYear());
+        values.put(Params.KEY_DESIGNATION, cvData.getDesignation());
+        values.put(Params.KEY_ORGANISATION_NAME, cvData.getOrganisationName());
+        values.put(Params.KEY_WORKING_PERIOD, cvData.getWorkingPeriod());
+        values.put(Params.KEY_FIELD_OF_INTEREST, cvData.getInterestedField());
+        values.put(Params.KEY_SKILLS, cvData.getSkills());
+        values.put(Params.KEY_HOBBY, cvData.getHobby());
+        values.put(Params.KEY_STRENGTH, cvData.getStrength());
+        values.put(Params.KEY_ACHIEVEMENT, cvData.getAchievement());
+        values.put(Params.KEY_CARRIER_OBJECTIVE, cvData.getCarrierObjective());
+        values.put(Params.KEY_TITLE, cvData.getTitle());
+        values.put(Params.KEY_PERIOD,cvData.getPeriod());
+        values.put(Params.KEY_ROLE, cvData.getRole());
+        values.put(Params.KEY_DESCRIPTION, cvData.getDescription());
+        values.put(Params.KEY_TEAM_SIZE, cvData.getTeamSize());
+
+        //Lets update now
+        return db.update(Params.TABLE_NAME1, values, Params.KEY_ID + "=?",
+                new String[]{String.valueOf(cvData.getId())});
+    }
+
+    public int getCount(){
+        String query = "SELECT  * FROM " + Params.TABLE_NAME1;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        return cursor.getCount();
+
+    }
+
+}
