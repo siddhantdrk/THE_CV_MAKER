48a2b1ef870504b8112befa2ee3331b52815ae1e
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-11-08 01:23:23 +0530

    activity_personal_details2.xml added

---
+++ app/src/main/java/com/example/thecvmaker/PersonalDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.thecvmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details2);
+    }
+}
