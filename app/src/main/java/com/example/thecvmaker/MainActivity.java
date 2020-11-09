93419a343460c2b61fc8967d7f74ce113e898881
Mandar16 <iit2019059@iiita.ac.in>
2020-11-09 18:53:14 +0530

    picking image from gallery

--- app/src/main/java/com/example/thecvmaker/MainActivity.java
+++ app/src/main/java/com/example/thecvmaker/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.thecvmaker;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
     TextView personalDetails;
@@ -13,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     TextView workExperience;
     TextView projectDetailContributions;
     TextView OtherSkills;
+    private static final int PICK_PHOTO = 1;
+    CircleImageView image;
+    Uri selectedImage;
 
 
     @Override
@@ -36,7 +51,44 @@ public class MainActivity extends AppCompatActivity {
         workExperience = findViewById(R.id.work_experience_txt);
         projectDetailContributions = findViewById(R.id.project_con_txt);
         OtherSkills = findViewById(R.id.other_skills_txt);
+        image = findViewById(R.id.upload_user_photo);
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PICK_PHOTO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_PHOTO);
+            } else {
+
+                Toast.makeText(getApplicationContext(), "You dont have permission to acess this media", Toast.LENGTH_SHORT);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            // String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(selectedImage);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                image.setImageBitmap(bitmap);
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
