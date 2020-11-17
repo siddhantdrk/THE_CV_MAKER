e625d8910a7aba310fa33b743412c461904297fe
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-11-18 00:29:09 +0530

    RecyclerView visibility error resolved in OthersAndSkillsActivity.java

--- app/src/main/java/com/example/thecvmaker/adapter/SkillsRvAdapter.java
+++ app/src/main/java/com/example/thecvmaker/adapter/SkillsRvAdapter.java
@@ -15,7 +15,7 @@ import com.example.thecvmaker.models.SkillsItem;
 
 import java.util.List;
 
-public class SkillsRvAdapter extends RecyclerView.Adapter<SkillsRvAdapter.SkillsViewholder> {
+public class SkillsRvAdapter extends RecyclerView.Adapter<SkillsRvAdapter.SkillsViewHolder> {
     Context context;
     List<SkillsItem> SkillsList;
 
@@ -26,13 +26,13 @@ public class SkillsRvAdapter extends RecyclerView.Adapter<SkillsRvAdapter.Skills
 
     @NonNull
     @Override
-    public SkillsViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public SkillsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.skills_others_item, parent, false);
-        return new SkillsViewholder(view);
+        return new SkillsViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SkillsViewholder holder, int position) {
+    public void onBindViewHolder(@NonNull SkillsViewHolder holder, int position) {
         holder.SkillHobbyText.setText(SkillsList.get(position).getHobby());
         holder.SkillDescriptionText.setText(SkillsList.get(position).getSkill_description());
 
@@ -48,14 +48,14 @@ public class SkillsRvAdapter extends RecyclerView.Adapter<SkillsRvAdapter.Skills
 
     @Override
     public int getItemCount() {
-        return 0;
+        return SkillsList.size();
     }
 
-    public class SkillsViewholder extends RecyclerView.ViewHolder {
+    public static class SkillsViewHolder extends RecyclerView.ViewHolder {
         TextView SkillHobbyText, SkillDescriptionText;
         ImageView removeImgBtn;
 
-        public SkillsViewholder(@NonNull View itemView) {
+        public SkillsViewHolder(@NonNull View itemView) {
             super(itemView);
 
             SkillHobbyText = itemView.findViewById(R.id.skill_hobby_text);
