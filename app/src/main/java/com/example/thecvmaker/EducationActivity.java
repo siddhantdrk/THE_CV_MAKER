2ab157dc692b16aa723ff3a24588e043b9dbb1b8
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-11-17 13:25:18 +0530

    isAllDetailsFilled modified in EducationActivity.java

--- app/src/main/java/com/example/thecvmaker/EducationActivity.java
+++ app/src/main/java/com/example/thecvmaker/EducationActivity.java
@@ -23,7 +23,6 @@ import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 public class EducationActivity extends AppCompatActivity {
 
@@ -32,7 +31,7 @@ public class EducationActivity extends AppCompatActivity {
     private Spinner eduDegreeSpinner;
     private TextView addEducationBtn;
     private MaterialButton nextToWorkExperience;
-    private List<EducationItem> EducationList;
+    private ArrayList<EducationItem> EducationList;
     private RecyclerView recyclerView;
     private EducationRvAdapter recyclerViewAdapter;
     private EducationItem educationItem;
