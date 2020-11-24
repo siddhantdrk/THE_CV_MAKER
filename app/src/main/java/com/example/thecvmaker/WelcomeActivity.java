739cd713e499d4837a0c7f89e9c9f2c578056e04
Siddhant Khobragade <siddhantdrk@gmail.com>
2020-11-24 19:49:43 +0530

    firebase login/SignUp Done

--- app/src/main/java/com/example/thecvmaker/WelcomeActivity.java
+++ app/src/main/java/com/example/thecvmaker/WelcomeActivity.java
@@ -2,22 +2,44 @@ package com.example.thecvmaker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private MaterialButton createCvButton,updateCvButton;
+    private MaterialButton createCvButton, updateCvButton;
+
+    public static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private String mUsername;
+    private TextView UserNameText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        Toolbar myToolbar = findViewById(R.id.welcome_toolbar);
+        setSupportActionBar(myToolbar);
+        mFirebaseAuth = FirebaseAuth.getInstance();
         initViews();
         createCvButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +49,32 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    // User is signed in
+                    OnSignInInitialize(firebaseUser.getDisplayName());
+                    UserNameText.setText(mUsername);
+                } else {
+                    // User is signed out
+                    // Choose authentication providers
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                    // Create and launch sign-in intent
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder().setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
         MyDbHelper db = new MyDbHelper(WelcomeActivity.this);
 
 
@@ -34,7 +82,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (db.getCount() == 0) {
-                    Toast.makeText(WelcomeActivity.this, "First Make Your Cv", Toast.LENGTH_SHORT);
+                    Toast.makeText(WelcomeActivity.this, "First Make Your Cv", Toast.LENGTH_SHORT).show();
                 } else {
                     Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
                     startActivity(intent);
@@ -44,10 +92,51 @@ public class WelcomeActivity extends AppCompatActivity {
         });
     }
 
-    private void initViews() {
-        createCvButton=findViewById(R.id.create_cv_btn);
-        updateCvButton=findViewById(R.id.update_cv_btn);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        UserNameText.setText(mUsername);
+    }
 
+    private void OnSignInInitialize(String displayName) {
+        mUsername = displayName;
+    }
+
+    private void initViews() {
+        createCvButton = findViewById(R.id.create_cv_btn);
+        updateCvButton = findViewById(R.id.update_cv_btn);
+        UserNameText = findViewById(R.id.user_name);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed In Successfully!!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Please Check your SigIn Details or Internet Connectivity and Retry!!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sign_out_app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.sign_out) {
+            AuthUI.getInstance().signOut(this);
+            Toast.makeText(this, "Signed Out.", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
